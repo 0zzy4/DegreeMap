69435fc890a4527c9e95891a78fc71b7e11667ee
0zzy4 <jessica.ozzy@yahoo.com>
2026-01-11 14:45:56 -0500

    style: fix Year border outline for mobile

--- src/components/Year.tsx
+++ src/components/Year.tsx
@@ -10,7 +10,7 @@ export default function Year({ year}: { year: string }) {
   return (
     <div className="border border-gray-300 rounded-lg mt-4"> {/* Year Container */}
 
-      <div className="flex items-baseline gap-3 border-l border-t border-r bg-gray-200 rounded-t-lg px-4 py-2"> {/* Year Header */}
+      <div className="flex items-baseline gap-3 border-l border-t border-r border-gray-200 bg-gray-200 rounded-t-lg px-4 py-2"> {/* Year Header */}
         <h2 className="font-bold text-gray-800">{year}</h2>
         <p className="text-gray-600">|</p>
         <p className="text-sm text-gray-600">{yearCredits} credits</p>
